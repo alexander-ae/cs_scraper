863d8bca325009e20ebed1deacab1b361004fcd8
__alexander__ <alexander.ayasca.esquives@gmail.com>
2020-05-28 11:27:23 -0500

    Add the original files

---
+++ scrapers/items.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class ProductItem(scrapy.Item):
+    store = scrapy.Field()
+    barcodes = scrapy.Field()
+    sku = scrapy.Field()
+    brand = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    package = scrapy.Field()
+    image_url = scrapy.Field()
+    branch = scrapy.Field()
+    stock = scrapy.Field()
+    price = scrapy.Field()
